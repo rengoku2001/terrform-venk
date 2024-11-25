0b436b5d2239386b669fdbb94199080ebc7d9eb5
venkatesh <venkateshjatavath2@gmail.com>
2024-11-25 16:28:52 +0530

    add-day-2

---
+++ day-2-basiccode/variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+    description = "ami value"
+    type = string
+    default = ""
+  
+}
+
+variable "instance_type" {
+    type = string
+    default = ""
+
+  
+}
+
+variable "key_name" {
+    type = string
+    default = ""
+  
+}
